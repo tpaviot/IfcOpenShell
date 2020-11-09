61ea3c44ae960358557c078d8e1982330180c07e
tpaviot <tpaviot@gmail.com>
2020-11-09 16:04:37 +0100

    test ifcconvert added to conda recipe

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -1,4 +1,11 @@
-dir
 cd ..\work\test
+
+echo Test import
 python -c "import ifcopenshell;import ifcopenshell.geom"
+
+echo Run unittestts
 python tests.py
+
+echo Test IfcConvert
+IfcConvert .\input\tesselated_faceset.ifc
+IfcConvert .\input\revit2011_wall1.ifc
--- conda/run_test.sh
+++ conda/run_test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 cd ../work/test
+
+echo -e "Test import"
 python -c "import ifcopenshell;import ifcopenshell.geom"
+
+echo -e "Run unittestts"
 python tests.py
+
+echo -e "Test IfcConvert"
+IfcConvert ./input/tesselated_faceset.ifc
+IfcConvert ./input/revit2011_wall1.ifc
