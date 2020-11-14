99d9d27735e7fdbb7cdf4b2df3c271e6e34da0ed
tpaviot <tpaviot@gmail.com>
2020-11-14 09:13:04 +0100

    added 2 ifccsv test in conda build

--- conda/run_test.bat
+++ conda/run_test.bat
@@ -9,3 +9,9 @@ python tests.py
 echo Test IfcConvert
 IfcConvert .\input\tesselated_faceset.ifc
 IfcConvert .\input\revit2011_wall1.ifc
+
+echo Test ifccsv.py
+cd ..\src\ifccsv
+
+python ifccsv.py -i ..\..\test\input\acad2010_walls.ifc -q ".IfcElement" -a "Name" "Description" --export -c out1.csv
+python ifccsv.py -i ..\..\test\input\TestModel_IFC4Add2.ifc -q ".IfcElement" -a "Name" "Description" --export -c out2.csv
--- conda/run_test.sh
+++ conda/run_test.sh
@@ -2,7 +2,7 @@
 cd ../work/test
 
 echo -e "Test import"
-python -c "import ifcopenshell;import ifcopenshell.geom"
+python -c "import ifcopenshell;import ifcopenshell.geom;print('imports ok')"
 
 echo -e "Run unittestts"
 python tests.py
@@ -10,3 +10,9 @@ python tests.py
 echo -e "Test IfcConvert"
 IfcConvert ./input/tesselated_faceset.ifc
 IfcConvert ./input/revit2011_wall1.ifc
+
+echo -e "Test ifccsv.py"
+cd ../src/ifccsv
+
+./ifccsv.py -i ../../test/input/acad2010_walls.ifc -q ".IfcElement" -a "Name" "Description" --export -c out1.csv
+./ifccsv.py -i ../../test/input/TestModel_IFC4Add2.ifc -q ".IfcElement" -a "Name" "Description" --export -c out2.csv
